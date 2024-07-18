b16a31ff7e444272e603bd96e12f363aec737c58
Al-Fhaigar J. Usman <93189177+rubickking04@users.noreply.github.com>
2024-07-18 15:07:21 +0800

    coded by artisandev
    added Vuetify JS in Laravel 11

---
+++ resources/js/App.vue
@@ -0,0 +1,37 @@
+<template>
+    <v-app  class="background scrollable-section" >
+        <v-main>
+            <router-view/>
+            <HelloWorld />
+            <v-btn color="primary">Hello Vuetify</v-btn>
+        </v-main>
+    </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+
+export default {
+    components: {
+        HelloWorld,
+    },
+    mounted: function() {
+        let elHtml = document.getElementsByTagName('html')[0]
+        elHtml.style.overflowY = 'hidden'
+    },
+    destroyed: function() {
+        let elHtml = document.getElementsByTagName('html')[0]
+        elHtml.style.overflowY = null
+    },
+};
+</script>
+
+<style scoped>
+.status-bar {
+    background-color: transparent !important;
+}
+.scrollable-section {
+    overflow-y: auto;
+    max-height: 100vh;
+}
+</style>
---
+++ resources/js/components/HelloWorld.vue
@@ -0,0 +1,12 @@
+<template>
+    <v-container>
+        <v-btn color="primary">Hello Vuetify</v-btn>
+        <h1>hello</h1>
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: 'HelloWorld',
+};
+</script>
