7628b40f0d535d633f819a1640b228c895df39e0
Al-Fhaigar J. Usman <93189177+artsndev@users.noreply.github.com>
2024-07-20 23:29:23 +0800

    coded by artisandev
    added User Authentication Routes

--- routes/api.php
+++ routes/api.php
@@ -29,3 +29,32 @@ Route::middleware(['auth:admin-api', 'throttle:1000,1'])->group(function () {
         Route::get('/admin/data', 'auth');
     });
 });
+
+
+/*
+|--------------------------------------------------------------------------
+| API Routes for User
+|--------------------------------------------------------------------------
+*/
+
+use App\Http\Controllers\API\User\Auth\LoginController as UserLoginController;
+use App\Http\Controllers\API\User\Auth\RegisterController as UserRegisterController;
+use App\Http\Controllers\API\User\DashboardController as UserDashboardController;
+
+// User Login Route
+Route::controller(UserLoginController::class)->group(function () {
+    Route::post('/user/login', 'login');
+});
+
+// User Register Route
+Route::controller(UserRegisterController::class)->group(function () {
+    Route::post('/user/register', 'register');
+});
+
+// Middleware Route API for User.
+Route::middleware(['auth:user-api', 'throttle:1000,1'])->group(function () {
+    // User Data Route
+    Route::controller(UserDashboardController::class)->group(function () {
+        Route::get('/user/data', 'auth');
+    });
+});
