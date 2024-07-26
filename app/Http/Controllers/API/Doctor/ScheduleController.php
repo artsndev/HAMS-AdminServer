a172de811c745da08984a21242127120b2de85ba
Al-Fhaigar J. Usman <93189177+artsndev@users.noreply.github.com>
2024-07-26 18:55:31 +0800

    coded by artisandev
    added Schedule Controller for Doctors.

---
+++ app/Http/Controllers/API/Doctor/ScheduleController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers\API\Doctor;
+
+use App\Models\Schedule;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class ScheduleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'date' => 'required|date',
+            'start_time' => 'required|date_format:H:i',
+            'end_time' => 'required|date_format:H:i|after:start_time',
+        ]);
+
+        $schedule = Schedule::create([
+            'doctor_id' => Auth::guard('doctor')->user()->id,
+            'date' => $request->date,
+            'start_time' => $request->start_time,
+            'end_time' => $request->end_time,
+        ]);
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
