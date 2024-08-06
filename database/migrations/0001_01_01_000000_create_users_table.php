a1f0b870f4558f92e434985589441c9016a1bab2
Al-Fhaigar J. Usman <93189177+artsndev@users.noreply.github.com>
2024-08-07 01:25:17 +0800

    coded by artisandev

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('birthdate');
+            $table->string('address');
+            $table->string('phone_number');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
